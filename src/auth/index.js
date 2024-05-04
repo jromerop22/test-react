3f4027f661b7c13236df2c96440ea27a04ee9c95
jromerop22 <jromerop99@gmail.com>
2024-05-04 13:07:54 -0500

    otros indices

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,5 +1,5 @@
 
 
-export * from './AuthContext';
-export * from './AuthProvider';
-export * from './authReducer';
+export * from './context';
+export * from './pages';
+export * from './types/types'
--- src/heroes/index.js
+++ src/heroes/index.js
@@ -1,2 +1,4 @@
-export * from './HeroCard'
-export * from './HeroList'
+
+
+export * from './pages';
+export * from './routes/HeroesRoutes';
